4185ac640e8eb77dbe50bf06e15326211af8749a
Guillermo <47510430+GuillermoGAndres@users.noreply.github.com>
2020-08-23 18:08:29 -0500

    Add files via upload

---
+++ builtools/ant/HolaMundo.java
@@ -0,0 +1,11 @@
+
+public class HolaMundo{
+
+    public static void main(String[] args){
+        while(! (args.length > 0) ){
+            System.out.println("Must need to write your name like first parameter");
+            return;
+        }
+        System.out.println("Hola mundo :D\nand hello " + args[0]);
+    }
+}
